af75f15b0637c4d6d6fc82ce86b349fd0f70823a
NoxNode <dungeonsaboss@live.com>
2015-09-10 22:39:12 -0700

    fixed Node.cpp in Umbrella of Doom source

--- Cpp_Umbrella_of_Doom/src/Node.cpp
+++ Cpp_Umbrella_of_Doom/src/Node.cpp
@@ -1,1 +1,24 @@
 #include "Node.h"
+#include <iostream>
+
+Node::Node() {
+	next = nullptr;
+}
+
+Node::~Node() {
+	if (next != nullptr)
+		delete next;
+}
+
+Node* Node::getNext() {
+	return next;
+}
+
+void Node::setNext(Node* node) {
+	next = node;
+}
+
+void Node::print() {
+	using namespace std;
+	cout << "I'm a node" << endl;
+}
